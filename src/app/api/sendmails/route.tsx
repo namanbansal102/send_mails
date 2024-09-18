b8d4472cbf36318f7214e0da90244f4b9986f3ef
Naman Bansal <namanbansal102@gmail.com>
2024-09-18 11:44:37 +0530

    Production Deployment

---
+++ src/app/api/sendmails/route.tsx
@@ -0,0 +1,45 @@
+import nodemailer from "nodemailer"
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: "redmi812345redmi@gmail.com",
+      pass: "eorv nkin ivnr pbqp"
+    },
+    secure: true,
+});
+
+export async function POST(req:Request,res:Response){
+    const p:any=res;
+    console.log(p);
+    
+    const data=await req.json();
+
+const func =async () => {
+const mailData = {
+    from: {
+        name: `Chitkara University`,
+        address: "redmi812345redmi@gmail.com"
+    },
+    to: "namanbansal102@gmail.com",
+    subject: `U Hostel Naman Bansal(2310990580) GatePass Application`,
+    html: `any Problem`,
+};
+
+await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err:any, info:any) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
+});
+
+};
+await func();
+    return Response.json({"success":true,"data":data.email})
+}
